d19928e6c093ad7bd5a39492ec810c4858a9a1ee
Patrick Speidel <pat1st@gmx.de>
2022-12-04 16:09:48 +0100

    done day 4 star 2

---
+++ Day_04/Assignments_2.py
@@ -0,0 +1,15 @@
+file1 = open('Day_04\inputProd.txt', 'r')
+Lines = file1.readlines()
+
+count = 0
+
+for line in Lines:
+    a, b = line.split(',')
+    a1, a2 = a.split('-')
+    b1, b2 = b.split('-')
+    if (int(a1) <= int(b1) and int(a2) >= int(b1)) or \
+            (int(b1) <= int(a1) and int(b2) >= int(a1)):
+        count += 1
+        print(a, b, count)
+
+print(count)

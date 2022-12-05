54a6c1b023bf3917e05ee5cad25d9b9551ed951c
Patrick Speidel <pat1st@gmx.de>
2022-12-05 22:44:33 +0100

    read start situation into array of arrays

--- Day_05/TowersOfExpedition.py
+++ Day_05/TowersOfExpedition.py
@@ -5,12 +5,25 @@ fileFull = file1.read()
 situation, actions = fileFull.split('\n\n')
 print(situation)
 
-situation = situation.split('\n')
-situMatrix = np.array([list(x) for x in situation])
-towArr = []
+situation = situation.split('\n') 
+situMatrixFull = np.array([list(x) for x in situation]) 
+situMatrixTarget = np.empty(
+    (situMatrixFull.shape[0]-1, int(situMatrixFull.shape[1]/3)), str)
+print('Matrix:', situMatrixTarget.shape[0], situMatrixTarget.shape[1])
 
-for line in range(1, situMatrix.shape[1], 4):
-     np = situMatrix[0:situMatrix.shape[0]-1,line]
-     towArr.append(np)
+for line in range(1, situMatrixFull.shape[1], 4):
+    stack = situMatrixFull[0:situMatrixFull.shape[0]-1, line]
+    situMatrixTarget[0:situMatrixFull.shape[0], int(line/4)] = stack
+
+print(situMatrixTarget) # 2D array of the situation
+
+towArr = []
+for stack in range(0, situMatrixTarget.shape[1]):
+    stackArr = []
+    for pos in range(situMatrixTarget.shape[0]-1, 0, -1):
+        value = situMatrixTarget[pos, stack]
+        if value != ' ':
+            stackArr.append(value)
+    towArr.append(stackArr)
 
 print(towArr)

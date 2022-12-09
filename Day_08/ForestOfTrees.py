e3bf1e123ac827c1492ab3859dd23f38eeeed495
Patrick Speidel <pat1st@gmx.de>
2022-12-10 00:46:43 +0100

    day 8 almost almost

--- Day_08/ForestOfTrees.py
+++ Day_08/ForestOfTrees.py
@@ -1,13 +1,13 @@
-file1 = open('Day_08\inputProd.txt', 'r')
+file1 = open('Day_08\inputTest.txt', 'r')
 Lines = file1.readlines()
 
 Forest = []
 for line in Lines:
-	TreeLine = []
-	for i in range(0, len(line)):
-		if len(line[i].strip()) != 0:
-			TreeLine.append(line[i])
-	Forest.append(TreeLine)
+    TreeLine = []
+    for i in range(0, len(line)):
+        if len(line[i].strip()) != 0:
+            TreeLine.append(line[i])
+    Forest.append(TreeLine)
 
 print(Forest)
 
@@ -15,42 +15,55 @@ ForestWidth = len(Forest)
 ForestHeight = len(Forest[0])
 VisibleTrees = 0
 
+
 def VisibleHorizontaly(x, y, Forest):
-	for i in range(0, ForestWidth):
-		if x!=i:
-			if Forest[i][y] > Forest[x][y]:
-				return False
-	return True
+    visible = True
+    for i in range(0, y):
+        if Forest[x][i] >= Forest[x][y]:
+            visible = False
+            continue
+
+    for i in range(y, ForestWidth):
+        if Forest[x][i] >= Forest[x][y]:
+            if visible == False: return False
+    return visible
 
 
-def VisibleVertically(x, y, Forest):
-	for i in range(0, ForestWidth):
-		if y != i:
-			if Forest[x][i] > Forest[x][y]:
-				return False
-	return True
+def VisibleVerticaly(x, y, Forest):
+    visible = True
+    for i in range(0, x):
+        if Forest[i][y] >= Forest[x][y]:
+            visible = False
+            continue
+
+    for i in range(x, ForestWidth):
+        if Forest[i][y] >= Forest[x][y]:
+            if visible == False:
+                return False
+    return visible
+
 
 VisibleForest = []
 
 for x in range(0, ForestWidth):
-	VisibleTreeLine = []
-	for y in range(0, ForestHeight):
-		print(Forest[x][y], end='')
-		VisibleTreeLine.append(' ')
-		if x==0 or x==ForestHeight-1 or y==0 or y==ForestWidth-1:
-			VisibleTrees += 1
-			VisibleTreeLine[y] = Forest[x][y]
-		elif VisibleHorizontaly(x, y, Forest) or VisibleVertically(x, y, Forest):
-			VisibleTrees += 1
-			VisibleTreeLine[y] = Forest[x][y]
-	print()
-	VisibleForest.append(VisibleTreeLine)
+    VisibleTreeLine = []
+    for y in range(0, ForestHeight):
+        print(Forest[x][y], end='')
+        VisibleTreeLine.append(' ')
+        if x == 0 or x == ForestHeight-1 or y == 0 or y == ForestWidth-1:
+            VisibleTrees += 1
+            VisibleTreeLine[y] = Forest[x][y]
+        elif VisibleHorizontaly(x, y, Forest) or VisibleVerticaly(x, y, Forest):
+            VisibleTrees += 1
+            VisibleTreeLine[y] = Forest[x][y]
+    print()
+    VisibleForest.append(VisibleTreeLine)
 print(VisibleTrees)
 
-#print(VisibleForest)
+# print(VisibleForest)
 for line in VisibleForest:
-	for i in range(0, len(line)):
-		print(line[i], end='')
-	print()
+    for i in range(0, len(line)):
+        print(line[i], end='')
+    print()
 
 print(VisibleTrees)
